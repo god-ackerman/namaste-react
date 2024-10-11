0e748a7bf1136bb401a3dd886cfb3b0aed941339
god-ackerman <redsl4p@gmail.com>
2024-10-11 22:44:11 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,28 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World from React!"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// To create nested HTML structure like in React:
+
+// <div id="parent">
+//   <div id="child">
+//     <h1>This is h1 tag!</h1>
+//   </div>
+// </div>
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "This is h1 tag!")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
