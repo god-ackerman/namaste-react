f23270d911dc7fd5f869a32cb5917a8fb019b714
god-ackerman <redsl4p@gmail.com>
2024-10-12 16:20:34 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading", xyz: "abc" },
@@ -14,15 +16,17 @@
 //   </div>
 // </div>
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "This is h1 tag!")
-  )
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "This is h1 tag!"),
+    React.createElement("h2", {}, "This is h2 tag!"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is h1 tag!"),
+    React.createElement("h2", {}, "This is h2 tag!"),
+  ]),
+]);
 
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
