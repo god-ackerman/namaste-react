e48d044ba1452496c096bb6cd7bdc768cf9a8f1d
god-ackerman <redsl4p@gmail.com>
2024-10-15 22:13:44 +0530

    episode-05

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://png.pngtree.com/png-clipart/20200727/original/pngtree-red-chilli-food-logo-design-your-company-png-image_5263986.jpg";
