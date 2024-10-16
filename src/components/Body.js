aa284be39e54df0197d9d9e63ff075d3e0e01d78
god-ackerman <redsl4p@gmail.com>
2024-10-16 21:49:49 +0530

    episode-05

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="restaurant-container">
-          {/* We need to use functional components for reusability  */}
-          {resList.map((res) => (
-            <RestaurantCard key={res?.info.id} resData={res} />
-          ))}
-        </div>
+  const [listOfRestaurants, setListofRestaurants] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            //Filter logic
+            const filteredList = listOfRestaurants.filter(
+              (res) => res?.info.avgRating > 4.4
+            );
+            setListofRestaurants(filteredList);
+          }}
+        >
+          Top rated restaurants
+        </button>
       </div>
-    );
-  };
+      <div className="restaurant-container">
+        {/* We need to use functional components for reusability  */}
+        {listOfRestaurants.map((res) => (
+          <RestaurantCard key={res?.info.id} resData={res} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
