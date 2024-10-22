dc1391fee707742919a08c20b192266b05896dc7
god-ackerman <redsl4p@gmail.com>
2024-10-22 19:06:15 +0530

    episode-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,25 @@
-const About = () => {
-    return (
-        <div>
-            <h1>Helloi</h1>
-        </div>
-    );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>Namaste React</h2>
+                <UserClass name={"Shashwat Chitransh"} location={"Bhopal"} />
+            </div>
+        );
+    }
+}
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: {this.props.name}</h2>
+        <h3>Location: {this.props.location}</h3>
+        <h4>Contact: god-ackerman</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
