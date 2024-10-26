327799e3911922aeb66b5534632be47a434f66f0
god-ackerman <redsl4p@gmail.com>
2024-10-26 17:24:31 +0530

    episode-09

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>
-            Online: {onlineStatus? "✅" : "❌"}
+            Online: {onlineStatus ? "✅" : "❌"}
           </li>
           <li>
             <Link to="/">Home</Link>
