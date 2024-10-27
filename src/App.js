dc77c129229a585ebe2319f901afd037e65bf7b7
god-ackerman <redsl4p@gmail.com>
2024-10-27 18:44:49 +0530

    episode-09

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
@@ -17,6 +16,9 @@ const AppLayout = () => {
   );
 };
 
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"))
+
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -28,11 +30,15 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />
+        element: <Suspense fallback={<h1>Loading...</h1>}><About /></Suspense>
       },
       {
         path: "/contact",
         element: <Contact />
+      },
+      {
+        path: "/grocery",
+        element: <Suspense fallback={<h1>Loading...</h1>}><Grocery /></Suspense>
       },
       {
         path: "/restaurants/:resId",
